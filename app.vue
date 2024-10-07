1c17fbe3d79cd45a13e0dd6a110015bad4a1f4a2
Zainab <pk.x3311@gmail.com>
2024-10-07 14:22:33 +0300

    sidebar mune done withe responsive and effects

--- app.vue
+++ app.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div :class="{ 'dark': darkMode }">
+    <div class='bg-white dark:bg-dim-900'>
+      <div class='min-h-full'>
+
+        <div class='grid grid-col-12 mx-auto sm:px-6 lg:px-8 lg:max-w-7xl lg:gap-5'>
+
+          <!-- Left Sidebar -->
+          <div class='md:block xs-col-span-1 xl:col-span-2'>
+            <div class='sticky top-0'>
+
+              <SidebarLeft />
+              <!-- <sidebar-left />  => It can be like this -->
+
+            </div>
+          </div>
+          <!-- Main Content -->
+          <!-- Right Sidebar -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script setup>
+const darkMode = ref(false)
+
+</script>
---
+++ components/Sidebar/Left/index.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class='flex flex-col h-screen'>
+
+        <div class="p-2 my-2 hover:bg-blue-50 rounded-full w-min dark:hover:bg-white/20" :class='defaultTransition'>
+            <nuxt-link to="/">
+                <div class='w-8 h-8'>
+                    <LogoTwitter />
+                </div>
+            </nuxt-link>
+        </div>
+        <div class="mt-2 space-y-8 ">
+
+            <SidebarLeftTap>
+                <template v-slot:icon>
+                    <HomeIcon />
+                </template>
+                <template v-slot:title>
+                    Home
+                </template>
+            </SidebarLeftTap>
+            <SidebarLeftTap>
+                <template v-slot:icon>
+                    <HashtagIcon />
+                </template>
+                <template v-slot:title>
+                    Explor
+                </template>
+            </SidebarLeftTap>
+            <SidebarLeftTap>
+                <template v-slot:icon>
+                    <BellIcon />
+                </template>
+                <template v-slot:title>
+                    Notifications
+                </template>
+            </SidebarLeftTap>
+            <SidebarLeftTap>
+                <template v-slot:icon>
+                    <InboxInIcon />
+                </template>
+                <template v-slot:title>
+                    BookMarks
+                </template>
+            </SidebarLeftTap>
+            <SidebarLeftTap>
+                <template v-slot:icon>
+                    <BookmarkIcon />
+                </template>
+                <template v-slot:title>
+                    Messages
+                </template>
+            </SidebarLeftTap>
+            <SidebarLeftTap>
+                <template v-slot:icon>
+                    <DocumentTextIcon />
+                </template>
+                <template v-slot:title>
+                    Lists
+                </template>
+            </SidebarLeftTap>
+            <SidebarLeftTap>
+                <template v-slot:icon>
+                    <UserIcon />
+                </template>
+                <template v-slot:title>
+                    Profile
+                </template>
+            </SidebarLeftTap>
+            <SidebarLeftTap>
+                <template v-slot:icon>
+                    <DotsCircleHorizontalIcon />
+                </template>
+                <template v-slot:title>
+                    More
+                </template>
+            </SidebarLeftTap>
+        </div>
+    </div>
+</template>
+<script setup>
+import { HomeIcon } from '@heroicons/vue/solid';
+import {HashtagIcon ,BellIcon,InboxInIcon ,BookmarkIcon,DocumentTextIcon,UserIcon,DotsCircleHorizontalIcon } from '@heroicons/vue/outline';
+const { defaultTransition } = useTailwindConfig();
+
+</script>
