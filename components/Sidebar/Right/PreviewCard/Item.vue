f0ded44a9b6a9833b5ee395a01ee8444d934abe5
Zainab <pk.x3311@gmail.com>
2024-10-08 12:38:04 +0300

    compolete right sidebar

---
+++ components/Sidebar/Right/PreviewCard/Item.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class='p-3 cursor-pointer hover:bg-gray-100 dark:hover:bg-dim-300' :class='defaultTransition'>
+        <slot></slot>
+    </div>
+
+</template>
+
+<script setup>
+const {defaultTransition, twitterBorderColor} = useTailwindConfig();
+</script>
